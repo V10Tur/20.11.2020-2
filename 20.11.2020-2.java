bb8ec19686b93a92c050855b79feb11164b44362
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 12:09:28 +0300

    Create 20.11.2020-2.java

---
+++ 20.11.2020-2.java
@@ -0,0 +1,17 @@
+
+
+
+    public class Program{
+
+        public static void main(String[] args) {
+
+            double radius = 60;
+            System.out.printf("Radisu: %f \n", radius);             // 60
+            System.out.printf("Area: %f \n", Math.PI * radius);     // 188,4
+        }
+    }
+    class Math{
+        public static final double PI = 3.14;
+    }
+
+
